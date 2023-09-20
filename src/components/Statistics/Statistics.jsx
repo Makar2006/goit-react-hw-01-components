d1c6bcc7079ade007ee38cf01f7212db547c00dd
Makar2006 <makar.diakiv@gmail.com>
2023-09-20 16:13:37 +0200

    final-2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.statisticsTitle}>{title}</h2>
+      <h2 className={title ? css.statisticsTitle : css.hidden}>{title}</h2>
 
       <ul className={css.statisticsStatList}>
         {stats.map(({ id, label, percentage }) => {
